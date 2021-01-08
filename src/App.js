ed0f7014205299a4022ad0c2e77aedc4aa06c0c2
ikbenwen <w.michels@novi-education.nl>
2021-01-08 20:43:34 +0100

    img aangepast

--- src/App.js
+++ src/App.js
@@ -1,59 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import './App.css';
-import axios from 'axios';
-import './App.css';
+import PokemonCard from "./components/pokemonCard";
+import 'tachyons';
 
 function App() {
-    const [pokemonData, setPokemonData] = useState(null);
-    async function fetchData() {
-        try {
-            const result = await axios.get(
-                'https://pokeapi.co/api/v2/pokemon/jigglypuff');
-            console.log(result.data);
-            setPokemonData(result.data);
-        } catch (e) {
-            console.error(e);
-        }
-    }
 
     return (
+        <>
+        <PokemonCard name="pikachu"/>
+        <PokemonCard name="jigglypuff"/>
+        </>
+        )
 
-            <div className="pokémonCard-container">
-                <h1> Pokémon </h1>
-
-                <div>
-                    <button
-                        type="button"
-                        onClick={fetchData}
-                    >
-                        find Pokémon!
-                    </button>
-                </div>
-
-
-                <div className="card-header">
-                    {pokemonData &&
-
-                    <h2>{pokemonData.name}</h2>}
-
-
-
-                    return ()
-
-                        <div className="pokemon" key={index}>
-                    <h3>Pokemon {index +1}</h3>
-                        <h2> {pokemonData.name}</h2>
-
-                        <div className="pokémon-details">
-                    <p>{abilities}</p>
-                        <p>{pokemonData.weight}</p>
-                        <p>{pokemonData.move}</p>
-                        </div>
-                        </div>
-
-
-
-
-                        );
-                        }
-                        export default App;
+}
+export default App;
---
+++ src/components/pokemonCard.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+// import './App.css';
+import axios from 'axios';
+import 'tachyons';
+
+
+export default function PokemonCard(props) {
+    const [pokemonData, setPokemonData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await axios.get(
+                    `https://pokeapi.co/api/v2/pokemon/${props.name}`
+                );
+                console.log(result.data);
+                setPokemonData(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        fetchData();
+    },[props.name]);
+
+
+    return (
+        <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5pokemon-Card-Container'>
+            <div className="pokemon-card">
+                {/*<h1>Pokémon</h1>*/}
+                <p>{pokemonData?.name}</p>
+                <img src={pokemonData?.sprites.front_default} alt={pokemonData?.name} />
+                <p>moves: {pokemonData?.moves.length+1}</p>
+                <p>weight: {pokemonData?.weight} kg</p>
+                <p id="abilities">Abilities:
+                    <ul>{pokemonData?.abilities.map((prop)=> {return<li key={prop.slot}>{prop.ability.name}</li>})}</ul>
+                </p>
+            </div>
+        </div>
+    )
+}
+
